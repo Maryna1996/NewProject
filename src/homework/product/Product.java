c96c50d682aa8bb9035245422055e1b690340f73
Maryna <133129896+Maryna1996@users.noreply.github.com>
2023-09-29 10:32:34 +0300

    Product commit app

--- src/homework/product/Product.java
+++ src/homework/product/Product.java
@@ -1,6 +1,9 @@
 package homework.product;
-
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 class Product {
     private int id;
@@ -8,22 +11,13 @@ class Product {
     private double price;
     private boolean discount;
     private LocalDate addDate;
-    private String category;
 
-    // Constructors, getters and setters
-
-    // Examples of constructors and getters/setters:
-    public Product(int id, String type, double price, boolean discount, LocalDate addDate, String category) {
+    public Product(int id, String type, double price, boolean discount, LocalDate addDate) {
         this.id = id;
         this.type = type;
         this.price = price;
         this.discount = discount;
         this.addDate = addDate;
-        this.category = category;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getType() {
@@ -34,27 +28,59 @@ class Product {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public boolean isDiscount() {
         return discount;
-    }
-
-    public void setDiscount(boolean discount) {
-        this.discount = discount;
     }
 
     public LocalDate getAddDate() {
         return addDate;
     }
 
-    public String getCategory() {
-        return category;
+    public static List<Product> filterExpensiveBooks(List<Product> products) {
+        return products.stream()
+                .filter(product -> "Book".equals(product.getType()) && product.getPrice() > 250)
+                .collect(Collectors.toList());
     }
 
-// Other methods and toString() for debugging
+    public static List<Product> filterDiscountedBooks(List<Product> products) {
+        return products.stream()
+                .filter(product -> "Book".equals(product.getType()) && product.isDiscount())
+                .map(product -> new Product(product.getId(), product.getType(), product.getPrice() * 0.9, product.isDiscount(), product.getAddDate()))
+                .collect(Collectors.toList());
+    }
+
+    private int getId() {
+        return 0;
+    }
+
+    public static Product findCheapestBook(List<Product> products) {
+        return products.stream()
+                .filter(product -> "Book".equals(product.getType()))
+                .min(Comparator.comparing(Product::getPrice))
+                .orElseThrow(() -> new RuntimeException("Продукт [категорія: Book] не знайдено"));
+    }
+
+    public static List<Product> findLatestProducts(List<Product> products, int count) {
+        return products.stream()
+                .sorted(Comparator.comparing(Product::getAddDate).reversed())
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+
+    public static double calculateTotalCost(List<Product> products) {
+        int currentYear = LocalDate.now().getYear();
+        return products.stream()
+                .filter(product -> product.getAddDate().getYear() == currentYear)
+                .filter(product -> "Book".equals(product.getType()))
+                .filter(product -> product.getPrice() <= 75)
+                .mapToDouble(Product::getPrice)
+                .sum();
+    }
+
+    public static Map<String, List<Product>> groupByType(List<Product> products) {
+        return products.stream()
+                .collect(Collectors.groupingBy(Product::getType));
+    }
 
     @Override
     public String toString() {
@@ -64,7 +90,6 @@ class Product {
                 ", price=" + price +
                 ", discount=" + discount +
                 ", addDate=" + addDate +
-                ", category='" + category + '\'' +
                 '}';
     }
 }
