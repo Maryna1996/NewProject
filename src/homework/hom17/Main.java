fb7bed8e91f48fc6aba9036a4e3ca53af3361bfa
Maryna <133129896+Maryna1996@users.noreply.github.com>
2023-10-16 13:12:02 +0300

    commit app

---
+++ src/homework/hom17/Main.java
@@ -0,0 +1,24 @@
+package homework.hom17;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws FileMaxSizeReachedException, IOException {
+        FileLoggerConfiguration config = new FileLoggerConfiguration(
+                "log.txt",
+                LoggingLevel.DEBUG,
+                1024,
+                "[TIME][LEVEL] Message: [MESSAGE]"
+        );
+
+        FileLogger logger = new FileLogger(config);
+
+        try {
+            logger.info("This is an info message.");
+            logger.debug("This is a debug message.");
+        } catch (IOException | FileMaxSizeReachedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
