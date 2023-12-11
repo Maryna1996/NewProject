277fa876602ee32046e45de558c28e0d110240ba
Maryna <133129896+Maryna1996@users.noreply.github.com>
2023-12-11 10:24:22 +0200

    final homework

---
+++ src/homework/Logger/coffee/Order.java
@@ -0,0 +1,39 @@
+package homework.Logger.coffee;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Order implements Comparable<Order> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Order.class);
+
+    private static int nextOrderNumber = 1;
+
+    private final int orderNumber;
+    private final String name;
+
+    public Order(String name) {
+        this.orderNumber = nextOrderNumber++;
+        this.name = name;
+        LOGGER.info("Creating Order instance with number {} and name {}", orderNumber, name);
+    }
+
+    public int getOrderNumber() {
+        LOGGER.info("Getting order number: {}", orderNumber);
+        return orderNumber;
+    }
+
+    public String getName() {
+        LOGGER.info("Getting order name: {}", name);
+        return name;
+    }
+
+    @Override
+    public int compareTo(Order other) {
+        return Integer.compare(this.orderNumber, other.orderNumber);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d | %s", orderNumber, name);
+    }
+}
